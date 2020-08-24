456b28bde59890418da203ba8eb316fb0a29cb94
Christoph Jerolimov <jerolimov+git+2020@gmail.com>
2020-08-24 23:45:59 +0200

    Add tekton-* namespaces as system namespaces

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,10 @@
-import { Namespace } from "./types";
+import { V1Namespace as Namespace } from '@kubernetes/client-node/dist/gen/model/v1Namespace';
 
 export function filterSystemNamespace(namespace: Namespace) {
-  const name = namespace.metadata.name;
-  return name.startsWith('kube-') || name.startsWith('kubernetes-');
+  const name = namespace.metadata?.name;
+  return name && (
+    name.startsWith('kube-') ||
+    name.startsWith('kubernetes-') ||
+    name.startsWith('tekton-')
+  );
 }
